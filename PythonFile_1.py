22522b8f6efe4ef2e33ae6d68eb14668ab3351a6
PrakharDutt <prakhar.dutt@bizacuity.com>
2023-03-17 16:07:40 +0530

    update 2

--- PythonFile_1.py
+++ PythonFile_1.py
@@ -17,3 +17,20 @@ def simple_interest(p,t,r):
 simple_interest(8, 6, 8)
 
 print("all done")
+
+
+
+# Python3 program to find compound
+# interest for given values.
+
+
+def compound_interest(principal, rate, time):
+
+	# Calculates compound interest
+	Amount = principal * (pow((1 + rate / 100), time))
+	CI = Amount - principal
+	print("Compound interest is", CI)
+
+
+# Driver Code
+compound_interest(10000, 10.25, 5)
